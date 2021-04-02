8a87b18cd2f903a563dd46b44a0c2320e87eb6d9
Lucas Cardoso Tavares <lucas_tavares@id.uff.br>
2021-04-02 16:19:21 -0300

    Ajeitando layout

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function selecionado(){
+var borda = document.querySelector(".prato");
+borda.classList.toggle("bordaverde");
+var tique = document.querySelector(".icone");
+tique.classList.toggle("vetor");
+}

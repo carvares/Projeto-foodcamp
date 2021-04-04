48a0cc311f49053942af3ffdb762f72a1b080108
Lucas Cardoso Tavares <lucas_tavares@id.uff.br>
2021-04-03 23:51:33 -0300

    menus prontos

--- scripts.js
+++ scripts.js
@@ -1,6 +1,54 @@
-function selecionado(){
-var borda = document.querySelector(".prato");
-borda.classList.toggle("bordaverde");
-var tique = document.querySelector(".icone");
-tique.classList.toggle("vetor");
+
+function selecionadoprato(escolha){
+    const selecaoprato = document.querySelector(".bordaverdeprato");
+    const selecaoicone  = document.querySelector(".mostrar");
+    const qualprato = "." + "comida" + escolha;
+    const qualtique = "." + "icone" + escolha;
+
+    const borda = document.querySelector(qualprato);
+    const tique = document.querySelector(qualtique);
+    
+    if (selecaoprato !== null){
+        selecaoprato.classList.remove("bordaverdeprato");
+        selecaoicone.classList.remove("mostrar");
+    }
+
+    borda.classList.add("bordaverdeprato");
+    tique.classList.add("mostrar");
+}
+
+function selecionadobebida(escolha){
+    const selecaobebida = document.querySelector(".bordaverdebebida");
+    const selecaoicone  = document.querySelector(".mostrar");
+    const qualbebida = "." + "bebida" + escolha;
+    const qualtique = "." + "icone" + escolha;
+
+    const borda = document.querySelector(qualbebida);
+    const tique = document.querySelector(qualtique);
+    
+    if (selecaobebida !== null){
+        selecaobebida.classList.remove("bordaverdebebida");
+        selecaoicone.classList.remove("mostrar");
+    }
+
+    borda.classList.add("bordaverdebebida");
+    tique.classList.add("mostrar");
+}
+
+function selecionadosobremesa(escolha){
+    const selecaosobremesa = document.querySelector(".bordaverdesobremesa");
+    const selecaoicone  = document.querySelector(".mostrar");
+    const qualsobremesa = "." + "sobremesa" + escolha;
+    const qualtique = "." + "icone" + escolha;
+
+    const borda = document.querySelector(qualsobremesa);
+    const tique = document.querySelector(qualtique);
+    
+    if (selecaosobremesa !== null){
+        selecaosobremesa.classList.remove("bordaverdesobremesa");
+        selecaoicone.classList.remove("mostrar");
+    }
+
+    borda.classList.add("bordaverdesobremesa");
+    tique.classList.add("mostrar");
 }
